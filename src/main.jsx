d0e6b6546f94aeb27002e21dc0dd1bcd851f084f
sofipfeffer <sofia.pf@outlook.com>
2024-08-05 14:56:40 -0300

    entregar hasta aca

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { UserProvider, UserContext } from './context/UserContext';
 import Router from './router/router.jsx';
 import NameForm from './views/NameForm/NameForm';
-import { ThemeProvider } from './context/ThemeContext.jsx';
-import FloatingButton from './components/FloatingButton/FloatingButton.jsx';
 import './index.css';
 
 const MainApp = () => {
@@ -14,17 +12,17 @@ const MainApp = () => {
     <div className="App">
       {!nombre && <NameForm />}
       {nombre && <Router />}
-      <FloatingButton />
+
     </div>
   );
 };
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <UserProvider>
-        <MainApp />
-      </UserProvider>
-    </ThemeProvider>
+
+    <UserProvider>
+      <MainApp />
+    </UserProvider>
+
   </React.StrictMode>,
 );
